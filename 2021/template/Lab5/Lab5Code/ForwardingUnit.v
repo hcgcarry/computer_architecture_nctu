223032c4b74a31dcb95708b2e2e1b7f4c0816b77
hcgcarry <hcgcarry@gmail.com>
2021-02-09 21:26:12 +0800

    add template

---
+++ 2021/template/Lab5/Lab5Code/ForwardingUnit.v
@@ -0,0 +1,9 @@
+module ForwardingUnit (instr_i,EXE_instr19_15, EXE_instr24_20, MEM_instr11_7, MEM_WBControl, WB_instr11_7, WB_Control, src1_sel_o, src2_sel_o);
+
+	input [31:0]instr_i;
+	input [5-1:0] EXE_instr19_15, EXE_instr24_20, MEM_instr11_7, WB_instr11_7;
+	input [2-1:0] MEM_WBControl, WB_Control;
+	output wire [2-1:0] src1_sel_o, src2_sel_o;
+
+endmodule
+ 
